dd09762b2c8e3b05f09c9c98dc7fbc8dd9eb5b55
Greg Meltzer <gregory.m.meltzer@gmail.com>
2023-08-11 08:37:28 -0400

    simplify will-mount

--- src/cljs/clj_trader/user_settings.cljs
+++ src/cljs/clj_trader/user_settings.cljs
@@ -13,20 +13,18 @@
 (defn refresh-settings-mixin []
   {:will-mount
    (fn [state]
-     (let [*data (atom nil)
-           comp (:rum/react-component state)]
-       (GET (str api/api-url "userSettings")
-            {:handler         (fn [data]
-                                (reset! *data data)
-                                (swap! component-state assoc :settings data)
-                                (rum/request-render comp))
-             :response-format :json
-             :keywords?       true})
-       (assoc state :initial-settings *data)))})
+     (prn "will mount")
+     (GET (str api/api-url "userSettings")
+          {:handler         (fn [data]
+                              (swap! component-state assoc :settings data)
+                              (swap! component-state assoc :initial-settings data))
+           :response-format :json
+           :keywords?       true})
+     state)})
 
 (defn handle-save []
   (PUT (str api/api-url "userSettings")
-       {:params (:settings @component-state)
+       {:params          (:settings @component-state)
         :handler         (fn [data]
                            (swap! component-state assoc :settings data)
                            (swap! component-state assoc :initial-settings data))
@@ -54,67 +52,65 @@
                                      (remove #{symbol} symbols)))}
                 "X")))
 
-(rum/defcs settings-panel < rum/reactive (refresh-settings-mixin) [state user-settings change-settings]
-  (let [initial-settings @(:initial-settings state)]
-    (swap! component-state assoc :initial-settings initial-settings)
+(rum/defc settings-panel < rum/reactive (refresh-settings-mixin) [user-settings change-settings]
+  (mui-x/stack
+    {:direction       "column"
+     :spacing         1
+     :justify-content "flex-start"
+     :align-items     "baseline"}
+    (mui/form-control-label {:label   "Enable Automated Trading"
+                             :control (mui/switch {:on-change #(swap! component-state
+                                                                      assoc-in
+                                                                      [:settings :enable-automated-trading]
+                                                                      (.. % -target -checked))
+                                                   :checked   (-> (rum/react component-state)
+                                                                  :settings
+                                                                  :enable-automated-trading)})})
+    (mui/form-control-label {:label   "Enable End of Day Exit"
+                             :control (mui/switch {:on-change #(swap! component-state
+                                                                      assoc-in
+                                                                      [:settings :end-of-day-exit]
+                                                                      (.. % -target -checked))
+                                                   :checked   (-> (rum/react component-state)
+                                                                  :settings
+                                                                  :end-of-day-exit)})})
+    (mui/textfield {:label       "Trading Frequency Seconds"
+                    :type        "number"
+                    :Input-Props {:inputProps {:min 1}}
+                    :on-change   #(swap! component-state
+                                         assoc-in
+                                         [:settings :trading-freq-seconds]
+                                         (.. % -target -valueAsNumber))
+                    :value       (or (-> (rum/react component-state)
+                                         :settings
+                                         :trading-freq-seconds) "")})
+    (mui/textfield {:label       "Position Size ($)"
+                    :type        "number"
+                    :Input-Props {:inputProps {:min 0}}
+                    :on-change   #(swap! component-state
+                                         assoc-in
+                                         [:settings :position-size]
+                                         (.. % -target -valueAsNumber))
+                    :value       (or (-> (rum/react component-state)
+                                         :settings
+                                         :position-size) "")})
+    (mui/textfield {:label       "Add New Symbol"
+                    :on-key-down handle-type-symbol
+                    :on-change   #(swap! component-state
+                                         assoc
+                                         :symbol
+                                         (.. % -target -value))
+                    :value       (or (:symbol (rum/react component-state)) "")})
+    (mui/list
+      (map render-symbol-item (-> (rum/react component-state)
+                                  :settings
+                                  :symbols)))
     (mui-x/stack
-      {:direction       "column"
-       :spacing         1
-       :justify-content "flex-start"
-       :align-items     "baseline"}
-      (mui/form-control-label {:label   "Enable Automated Trading"
-                               :control (mui/switch {:on-change #(swap! component-state
-                                                                        assoc-in
-                                                                        [:settings :enable-automated-trading]
-                                                                        (.. % -target -checked))
-                                                     :checked   (-> (rum/react component-state)
-                                                                    :settings
-                                                                    :enable-automated-trading)})})
-      (mui/form-control-label {:label   "Enable End of Day Exit"
-                               :control (mui/switch {:on-change #(swap! component-state
-                                                                        assoc-in
-                                                                        [:settings :end-of-day-exit]
-                                                                        (.. % -target -checked))
-                                                     :checked   (-> (rum/react component-state)
-                                                                    :settings
-                                                                    :end-of-day-exit)})})
-      (mui/textfield {:label       "Trading Frequency Seconds"
-                      :type        "number"
-                      :Input-Props {:inputProps {:min 1}}
-                      :on-change   #(swap! component-state
-                                           assoc-in
-                                           [:settings :trading-freq-seconds]
-                                           (.. % -target -valueAsNumber))
-                      :value       (or (-> (rum/react component-state)
-                                           :settings
-                                           :trading-freq-seconds) "")})
-      (mui/textfield {:label       "Position Size ($)"
-                      :type        "number"
-                      :Input-Props {:inputProps {:min 0}}
-                      :on-change   #(swap! component-state
-                                           assoc-in
-                                           [:settings :position-size]
-                                           (.. % -target -valueAsNumber))
-                      :value       (or (-> (rum/react component-state)
-                                           :settings
-                                           :position-size) "")})
-      (mui/textfield {:label       "Add New Symbol"
-                      :on-key-down handle-type-symbol
-                      :on-change   #(swap! component-state
-                                           assoc
-                                           :symbol
-                                           (.. % -target -value))
-                      :value       (or (:symbol (rum/react component-state)) "")})
-      (mui/list
-        (map render-symbol-item (-> (rum/react component-state)
-                                    :settings
-                                    :symbols)))
-      (mui-x/stack
-        {:direction "row"
-         :spacing   1}
-        (mui/button {:variant  "contained"
-                     :on-click handle-save}
-                    "Save")
-        (mui/button {:variant  "contained"
-                     :on-click #(swap! component-state assoc :settings (:initial-settings @component-state))}
-                    "Reset")))))
+      {:direction "row"
+       :spacing   1}
+      (mui/button {:variant  "contained"
+                   :on-click handle-save}
+                  "Save")
+      (mui/button {:variant  "contained"
+                   :on-click #(swap! component-state assoc :settings (:initial-settings @component-state))}
+                  "Reset"))))
