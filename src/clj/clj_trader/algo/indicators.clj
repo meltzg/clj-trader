690156fda0ab04ed0f4791aea5aae58dc8953048
Greg Meltzer <gregory.m.meltzer@gmail.com>
2023-08-30 13:33:54 -0400

    functions can be higher order

--- src/clj/clj_trader/algo/indicators.clj
+++ src/clj/clj_trader/algo/indicators.clj
@@ -1,10 +1,42 @@
-(ns clj-trader.algo.indicators)
+(ns clj-trader.algo.indicators
+  (:import (java.lang Math)))
+
+(def config-map {:sma       {:name "Simple Moving Average"
+                              :opts {:periods {:type :int
+                                               :min  1}}}
+                  :bollinger {:name "Bollinger Bands"
+                              :opts {:periods      {:type :int
+                                                    :min  1}
+                                     :num-std-devs {:type :int
+                                                    :min  1}}}})
 
 (defn average [col]
-  (/ (apply + col) (count col)))
+  (/ (reduce + col) (count col)))
 
-(defn calculate-indicator [price-candles price-key window-size fn]
-  (->> price-candles
-       (map price-key)
-       (partition window-size 1)
-       (map fn)))
+(defn standard-deviation [col]
+  (let [avg (average col)]
+    (Math/sqrt
+      (/ (reduce + (map #(Math/pow (- % avg) 2) col))
+         (- (count col) 1)))))
+
+(defn bollinger-bands [num-std-devs col]
+  (let [avg (average col)
+        std-dev (standard-deviation col)
+        deviation (* num-std-devs std-dev)]
+    [(+ avg deviation)
+     (- avg deviation)]))
+
+(defmulti make-indicator :indicator)
+
+(defmethod make-indicator :sma [_]
+  average)
+
+(defmethod make-indicator :bollinger [{:keys [num-std-devs]}]
+  (partial bollinger-bands num-std-devs))
+
+(defn moving-indicator [{:keys [price-candles price-key window-size] :as op}]
+  (let [indicator (make-indicator op)]
+    (->> price-candles
+         (map price-key)
+         (partition window-size 1)
+         (map indicator))))
