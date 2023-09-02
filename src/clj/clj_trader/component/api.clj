07bd748737b6aef686bd9402c0a86b23de16eb54
Greg Meltzer <gregory.m.meltzer@gmail.com>
2023-09-02 15:00:21 -0400

    higher order functions are so hot

--- src/clj/clj_trader/component/api.clj
+++ src/clj/clj_trader/component/api.clj
@@ -11,10 +11,37 @@
             [ring.middleware.json :as json-mid]
             [ring.util.response :refer [resource-response response]]))
 
-(defn- sign-in-handler [config {:keys [td-brokerage]} {:keys [body]}]
-  (let [code (:code body)]
-    (response (-> (td/execute-command {:command      :sign-in
-                                       :code         code
+(defn- sign-in-handler [config {:keys [td-brokerage]}]
+  (fn [{:keys [body]}]
+    (let [code (:code body)]
+      (response (-> (td/execute-command {:command      :sign-in
+                                         :code         code
+                                         :config       config
+                                         :td-brokerage td-brokerage})
+                    (select-keys [:expires-at :refresh-expires-at :signed-in?])
+                    (update-vals #(if (instance? org.joda.time.DateTime %)
+                                    (tc/to-string %)
+                                    %)))))))
+
+(defn- get-auth-status-handler [config {:keys [td-brokerage]}]
+  (fn [& _]
+    (let [current-status (td/execute-command {:command      :auth-status
+                                              :config       config
+                                              :td-brokerage td-brokerage})
+          final-status (if (:signed-in? current-status)
+                         current-status
+                         (td/execute-command {:command      :refresh-access-token
+                                              :config       config
+                                              :td-brokerage td-brokerage}))]
+      (response (-> final-status
+                    (select-keys [:expires-at :refresh-expires-at :signed-in?])
+                    (update-vals #(if (instance? org.joda.time.DateTime %)
+                                    (tc/to-string %)
+                                    %)))))))
+
+(defn- sign-out-handler [config {:keys [td-brokerage]}]
+  (fn [& _]
+    (response (-> (td/execute-command {:command      :sign-out
                                        :config       config
                                        :td-brokerage td-brokerage})
                   (select-keys [:expires-at :refresh-expires-at :signed-in?])
@@ -22,46 +49,23 @@
                                   (tc/to-string %)
                                   %))))))
 
-(defn- get-auth-status-handler [config {:keys [td-brokerage]} & _]
-  (let [current-status (td/execute-command {:command      :auth-status
-                                            :config       config
-                                            :td-brokerage td-brokerage})
-        final-status (if (:signed-in? current-status)
-                       current-status
-                       (td/execute-command {:command      :refresh-access-token
-                                            :config       config
-                                            :td-brokerage td-brokerage}))]
-    (response (-> final-status
-                  (select-keys [:expires-at :refresh-expires-at :signed-in?])
-                  (update-vals #(if (instance? org.joda.time.DateTime %)
-                                  (tc/to-string %)
-                                  %))))))
-
-(defn- sign-out-handler [config {:keys [td-brokerage]} & _]
-  (response (-> (td/execute-command {:command      :sign-out
-                                     :config       config
-                                     :td-brokerage td-brokerage})
-                (select-keys [:expires-at :refresh-expires-at :signed-in?])
-                (update-vals #(if (instance? org.joda.time.DateTime %)
-                                (tc/to-string %)
-                                %)))))
-
-(defn- price-history-handler [config {:keys [td-brokerage]} params & _]
-  (response (td/execute-command {:command      :price-history
-                                 :config       config
-                                 :td-brokerage td-brokerage
-                                 :params       (merge params
-                                                      (update-vals
-                                                        (select-keys params [:period-type :frequency-type])
-                                                        keyword))})))
+(defn- price-history-handler [config {:keys [td-brokerage]} params]
+  (fn [& _]
+    (response (td/execute-command {:command      :price-history
+                                   :config       config
+                                   :td-brokerage td-brokerage
+                                   :params       (merge params
+                                                        (update-vals
+                                                          (select-keys params [:period-type :frequency-type])
+                                                          keyword))}))))
 
 (defn- app-routes [td-brokerage config]
   (routes
     (GET "/api/oauthUri" [] (fn [_] (response {:oauth-uri (get-in td-brokerage [:td-brokerage :oauth-uri])})))
     (GET "/" [] (resource-response "index.html" {:root "public"}))
-    (POST "/api/signIn" [] (partial sign-in-handler config td-brokerage))
-    (GET "/api/authStatus" [] (partial get-auth-status-handler config td-brokerage))
-    (GET "/api/signOut" [] (partial sign-out-handler config td-brokerage))
+    (POST "/api/signIn" [] (sign-in-handler config td-brokerage))
+    (GET "/api/authStatus" [] (get-auth-status-handler config td-brokerage))
+    (GET "/api/signOut" [] (sign-out-handler config td-brokerage))
     (GET "/api/userSettings" [] (fn [_] (response (-> config
                                                       :user-settings
                                                       deref))))
@@ -70,7 +74,7 @@
                                   (response (-> config
                                                 :user-settings
                                                 deref))))
-    (GET "/api/priceHistory" {params :params} (partial price-history-handler config td-brokerage params))
+    (GET "/api/priceHistory" {params :params} (price-history-handler config td-brokerage params))
     (GET "/api/periodFrequencyInfo" [] (fn [_] (response td/period-frequency-info)))
     (GET "/api/indicatorConfigInfo" [] (fn [_] (response indicators/config-map)))
     (route/resources "/")
