54ecf5cbd2b352230ce1b1f6c9bf166a5fc45f0c
Greg Meltzer <gregory.m.meltzer@gmail.com>
2023-09-04 22:29:41 -0400

    clean up dev code

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -2,3 +2,11 @@
   (:require [com.stuartsierra.component :as component]
             [clj-trader.core :as core]
             [fiddle]))
+
+(def sys (core/new-system "app.dev.edn" "user.dev.edn"))
+
+(defn start-system []
+  (alter-var-root #'sys component/start))
+
+(defn stop-system []
+  (alter-var-root #'sys component/stop))
